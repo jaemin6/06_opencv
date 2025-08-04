da5eb1e4b4199bc3191f3617f901c72df6b36a67
jaemin6 <gumt555@gmail.com>
2025-08-04 16:09:42 +0900

    트랙커 객체 함수 변경 openvv 4.x 버전용 cv2.legacy

--- src/track_trackingAPI.py
+++ src/track_trackingAPI.py
@@ -1,14 +1,15 @@
 import cv2
 
 # 트랙커 객체 생성자 함수 리스트 ---①
-trackers = [cv2.TrackerBoosting_create,
-            cv2.TrackerMIL_create,
-            cv2.TrackerKCF_create,
-            cv2.TrackerTLD_create,
-            cv2.TrackerMedianFlow_create,
-            cv2.TrackerGOTURN_create, #버그로 오류 발생
-            cv2.TrackerCSRT_create,
-            cv2.TrackerMOSSE_create]
+trackers = [cv2.legacy.TrackerBoosting_create,
+            cv2.legacy.TrackerMIL_create,
+            cv2.legacy.TrackerKCF_create,
+            cv2.legacy.TrackerTLD_create,
+            cv2.legacy.TrackerMedianFlow_create,
+            # cv2.legacy.TrackerGOTURN_create,  # GOTURN은 모델파일 없으면 오류
+            cv2.legacy.TrackerCSRT_create,
+            cv2.legacy.TrackerMOSSE_create]
+
 trackerIdx = 0  # 트랙커 생성자 함수 선택 인덱스
 tracker = None
 isFirst = True
